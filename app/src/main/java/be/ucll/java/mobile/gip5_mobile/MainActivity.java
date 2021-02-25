778435d8d3cbb37cc2857b7291d72bb12e5daf8e
jef <38948299+SimplyJeevS@users.noreply.github.com>
2021-02-25 17:52:40 +0100

    Added SettingsActivity and 3dot menu

--- app/src/main/java/be/ucll/java/mobile/gip5_mobile/MainActivity.java
+++ app/src/main/java/be/ucll/java/mobile/gip5_mobile/MainActivity.java
@@ -2,7 +2,11 @@ package be.ucll.java.mobile.gip5_mobile;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            Intent i = new Intent(this, SettingsActivity.class);
+            startActivity(i);
+            return true;
+        } else if (id == R.id.action_website){
+            // Replace with website url to front end ?
+            Intent web = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.ucll.be"));
+            startActivity(web);
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
